86b8a90d69f49275c445cbdaecbd772db0a6a768
Donald Hays <donald.hays@gmail.com>
2026-03-03 23:28:18 -0800

    All the things

---
+++ conf.lua
@@ -0,0 +1,11 @@
+function love.conf(t)
+    t.window.msaa = 4
+    t.window.highdpi = true
+
+    t.window.width = 1280
+    t.window.height = 720
+
+    t.window.minwidth = 500
+    t.window.minheight = 400
+    t.window.resizable = true
+end
---
+++ main.lua
@@ -0,0 +1,50 @@
+local iui = require "lib.iui"
+local backend = require "lib.love-iui"
+
+local sampleMain = require "sample"
+
+function love.load()
+    iui.load(backend)
+end
+
+function love.update(dt)
+    iui.beginFrame(dt)
+    iui.beginWindow(love.graphics.getDimensions())
+
+    sampleMain()
+
+    iui.endWindow()
+    iui.endFrame()
+end
+
+function love.draw()
+    iui.draw()
+end
+
+function love.mousemoved(x, y, dx, dy)
+    backend.mousemoved(x, y, dx, dy)
+end
+
+function love.mousepressed(x, y, button)
+    backend.mousepressed(x, y, button)
+end
+
+function love.mousereleased(x, y, button)
+    backend.mousereleased(x, y, button)
+end
+
+function love.wheelmoved(x, y)
+    backend.wheelmoved(x, y)
+end
+
+function love.keypressed(key, scancode, isRepeat)
+    backend.keypressed(key, scancode, isRepeat)
+end
+
+function love.keyreleased(key, scancode)
+    backend.keyreleased(key, scancode)
+end
+
+function love.textinput(text)
+    backend.textinput(text)
+end
